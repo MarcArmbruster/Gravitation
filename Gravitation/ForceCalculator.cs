68ed10b3ae8d0e29106c54db7e22057c4e0f3465
Marc Armbruster <marcarmbruster@hotmail.de>
2023-11-17 16:59:28 +0100

    Add project files.

---
+++ Gravitation/ForceCalculator.cs
@@ -0,0 +1,75 @@
+﻿namespace Gravitation
+{
+    using System;
+    
+    public class ForceCalculator
+    {
+        /// <summary>
+        /// Value Unit: m³/(kg s²)
+        /// </summary>
+        private static double GravitationConstant =  6.672 * Math.Pow(10, -11);
+        
+        /// <summary>
+        /// Force value in N
+        /// </summary>
+        /// <param name="massOne">Mass of one object</param>
+        /// <param name="massTwo">Mass of another object</param>
+        /// <param name="distance">Distance between the objects</param>
+        /// <returns>The gravitation force.</returns>
+        public double OutsideGravitationForce(double massOne, double massTwo, double distance)
+        {
+            if (distance < 0d)
+            {
+                throw new InvalidOperationException("Distance must be positive");
+            }
+            
+            if (massOne == 0d || massTwo == 0d)
+            {
+                throw new InvalidOperationException("Gravitation works only on objects with a mass");
+            }
+            else
+            {
+                return GravitationConstant * massOne * massTwo / (distance * distance);
+            }
+        }
+
+        /// <summary>
+        /// Force value in N if the second object is within the first object
+        /// </summary>
+        /// <param name="massOne">Mass of a big object</param>
+        /// <param name="massTwo">Mass of another (smaller) object</param>
+        /// <param name="massOneRadius">Radius of the greater object (object one)</param>
+        /// <param name="innerDistance">The distinct of the second object from center of first object</param>
+        /// <returns>The gravitation force.</returns>
+        public double InsideGravitationForce(
+            double massOne, 
+            double massTwo, 
+            double massOneRadius,
+            double innerDistance)
+        {
+            if (innerDistance < 0d)
+            {
+                throw new InvalidOperationException("Distance must be positive");
+            }
+
+            if (massOne == 0d || massTwo == 0d)
+            {
+                throw new InvalidOperationException("Gravitation works only on objects with a mass");
+            }
+
+            if (massOneRadius < innerDistance)
+            {
+                return OutsideGravitationForce(massOne, massTwo, innerDistance);
+            }
+            
+            if (massOne == 0d || massTwo == 0d)
+            {
+                throw new InvalidOperationException("Gravitation works only on objects with a mass");
+            }
+            else
+            {
+                return GravitationConstant * massOne * massTwo * innerDistance / (massOneRadius * massOneRadius * massOneRadius);
+            }
+        }
+    }
+}
---
+++ Gravitation/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Gravitation
+{
+    using System;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ForceCalculator forceCalculator = new ForceCalculator();
+            double earthRadius = 6_371_000; // average value in meters
+            double mountEverest = 6_371_000 + 8_848;
+            double earthMass = 5.9722d * Math.Pow(10, 24);
+            double objectWeight = 1d; // 1kg
+
+            double force = forceCalculator.OutsideGravitationForce(earthMass, objectWeight, earthRadius);
+            Console.WriteLine($"The average force value of 1kg is: {force} N");
+
+            double everestForce = forceCalculator.OutsideGravitationForce(earthMass, objectWeight, mountEverest);
+            Console.WriteLine($"The force value on Mount Everst of 1kg is: {everestForce} N");
+
+            double insideForce = forceCalculator.InsideGravitationForce(earthMass, objectWeight, earthRadius, 5_371_000);
+            Console.WriteLine($"The force value of 1kg in depth of 1000m is: {insideForce} N");
+
+            Console.ReadLine();
+        }
+    }
+}
